7254cf72bb0ff7ce75d97e7f76260fa191b2be8a
teamkweku <teamkweku@outlook.com>
2024-03-10 00:21:44 +0000

    creating the redis utils

---
+++ utils/redis.js
@@ -0,0 +1,38 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor () {
+    this.client = createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (err) => {
+      console.log(
+        'Redis client failed to connect:',
+        err.message || err.toString()
+      );
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+  }
+
+  isAlive () {
+    return this.isClientConnected;
+  }
+
+  async get (key) {
+    return promisify(this.client.get).bind(this.client)(key);
+  }
+
+  async set (key, value, duration) {
+    await promisify(this.client.setex).bind(this.client)(key, duration, value);
+  }
+
+  async del (key) {
+    await promisify(this.client.del).bind(this.client)(key);
+  }
+}
+
+export const redisClient = new RedisClient();
+export default redisClient;

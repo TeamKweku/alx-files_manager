25ebb268cf350a1f5008d1645629cb23fec1bc3d
teamkweku <teamkweku@outlook.com>
2024-03-10 10:03:31 +0000

    MongoDB dbClient utils implmentation

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const dbHost = process.env.DB_HOST || 'localhost';
+    const dbPort = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const dbURL = `mongodb://${dbHost}:${dbPort}/${database}`;
+
+    this.client = new MongoClient(dbURL, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+
+  async usersCollection() {
+    return this.client.db().collection('users');
+  }
+
+  async filesCollection() {
+    return this.client.db().collection('files');
+  }
+}
+
+export const dbClient = new DBClient();
+export default dbClient;
